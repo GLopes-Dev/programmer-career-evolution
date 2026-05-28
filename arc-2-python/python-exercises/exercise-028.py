0740ea415ba852286b5cd58a79b20c954cbc000b
GLopes-Dev <gsflopes2017@gmail.com>
2026-05-28 17:38:03 -0300

    Solved Exercise

--- arc-2-python/python-exercises/exercise-028.py
+++ arc-2-python/python-exercises/exercise-028.py
@@ -1,3 +1,11 @@
 #Challenge 028 - Create a program that make the computer pick a integer number between 0 and 5 and ask the user try to guess what number was chosen by the computer.
 # The program may display on the screen whether the user won or lost
-
+import random
+print("=" * 10, "[Challenge 028]", "=" * 10)
+num = int(random.randint(0, 5))
+choice = int(input("Pick up a number between 0 and 5: "))
+print(num)
+if choice == num:
+    print("Congratulations! You got it right! ")
+else:
+    print("You got it wrong. Try Again!")
