26910784b0b47370fd0338fd68ea29f2317acfa8
GLopes-Dev <gsflopes2017@gmail.com>
2026-05-28 18:08:22 -0300

    Solved Exercise

--- arc-2-python/python-exercises/exercise-029.py
+++ arc-2-python/python-exercises/exercise-029.py
@@ -1,3 +1,12 @@
 #Challenge 029 - Create a program that reads a car velocity. If it exceeds 80Km/h, display a message saying that he was fined.
 # The fine costs R$7,00 for each Km above the limit
 
+print("=" * 10, "[Challenge 029]", "=" * 10)
+vel = float(input("Car Velocity: "))
+if vel > 80:
+    abvl = vel - 80
+    fine = abvl * 7
+    print(f"You got fined.\nPrice: {fine:.2f}")
+else:
+    print("Safe.")
+print("=" * 30)
