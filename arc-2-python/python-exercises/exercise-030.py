40c877447492ad2696269b582bc4605164464ace
GLopes-Dev <gsflopes2017@gmail.com>
2026-05-30 10:41:30 -0300

    Solved Exercise

--- arc-2-python/python-exercises/exercise-030.py
+++ arc-2-python/python-exercises/exercise-030.py
@@ -1,3 +1,9 @@
 #Challenge 030 - Create a program that reads a integer number and displays on the screen if it is EVEN or ODD.
 #
 
+print("=" * 10, "[Challenge 030]", "=" * 10)
+num = int(input('Type any number: '))
+if num % 2 == 0:
+    print(f"The number {num} is EVEN")
+else:
+    print(f"The number {num} is ODD")
