1db9af04734e462878f6aa423d993344e4581db0
GLopes-Dev <gsflopes2017@gmail.com>
2026-05-30 11:39:58 -0300

    Solved Exercise

--- arc-2-python/python-exercises/exercise-032.py
+++ arc-2-python/python-exercises/exercise-032.py
@@ -1,1 +1,9 @@
 #Challenge 032 - Create a program that reads a random year and display on the screen if it's a leap year.
+
+print("=" * 10, "[Challenge 032]", "=" * 10)
+year = int(input('Type a Year: '))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year. ")
+else:
+    print(f"{year} isn't a leap year. ")
