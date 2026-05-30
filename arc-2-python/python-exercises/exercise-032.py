13cedbf35f6672d1ff278b51d6a3a88685c2caa1
GLopes-Dev <gsflopes2017@gmail.com>
2026-05-30 15:34:01 -0300

    Edited Exercise

--- arc-2-python/python-exercises/exercise-032.py
+++ arc-2-python/python-exercises/exercise-032.py
@@ -1,8 +1,10 @@
 #Challenge 032 - Create a program that reads a random year and display on the screen if it's a leap year.
-
+from datetime import datetime
 print("=" * 10, "[Challenge 032]", "=" * 10)
-year = int(input('Type a Year: '))
-
+year = int(input('Type a Year (Enter 0 to analyze the current year): '))
+if year == 0:
+    current_moment = datetime.now()
+    year = int(current_moment.year)
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(f"{year} is a leap year. ")
 else:
