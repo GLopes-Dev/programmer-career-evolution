402b03a7ff77e822bfe501c7ce59bde47434f05d
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-04 09:38:49 -0300

    New Folder

---
+++ brazillian-informatic-olympics-2026/training-01.py
@@ -0,0 +1,35 @@
+'''a, b, c = map(int, input().split())
+print(a, b, c)
+print(a)
+print(b)
+print(c)'''
+
+
+#Little Challenge - 01
+# 1 - Abertas
+# 0 - Fechadas
+
+
+
+P, R = map(int, input().split())
+
+if P == 0:
+    print('C')
+elif P == 1:
+    if R == 0:
+        print('B')
+    else:
+        print('A')
+
+#Little Challenge - 02
+#Vitoria = 3 pontos | Empate = 1 ponto | Derrota = 0 pontos
+#
+
+Cv, Ce, Cs, Fv, Fe, Fs = map(int, input().split())
+if Cv + Ce == Fv + Fe:
+    if Cs > Fs:
+        print('C')
+    elif Fs > Cs:
+        print('F')
+    else:
+        print('=')
