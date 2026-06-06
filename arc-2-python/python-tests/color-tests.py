ad375ddd89fd26d962495adf2a62978adcaa1c94
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-06 12:19:44 -0300

    New Skills

---
+++ arc-2-python/python-tests/color-tests.py
@@ -0,0 +1,12 @@
+
+
+#print('\033[1;37m <-' * 25)
+#rint("\033[1;37m A terrible presence approachs you...", "\033[4;31m IT'S THE FLOREST LORD!!")
+#input()
+
+
+colors = {'clear':'\033[m', 'blue':'\033[34m', 'red':'\033[31m'}
+
+name = 'Gabriel'
+
+print(f'Nice to meet you, {colors['blue']}{name}{colors['clear']}!!')
