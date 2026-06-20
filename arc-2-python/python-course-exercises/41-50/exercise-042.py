65612cabbf19cd491820ae0d0ec016546c98fd29
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-20 10:01:51 -0300

    solved

--- arc-2-python/python-course-exercises/41-50/exercise-042.py
+++ arc-2-python/python-course-exercises/41-50/exercise-042.py
@@ -9,9 +9,9 @@ s2 = int(input('Side 2: '))
 s3 = int(input('Side 3: '))
 if s1 + s2 <= s3 or s1 + s3 <= s2 or s3 + s2 <= s1:
     print('The segments above can not form a Triangle :(')
-elif s1 == s2 and s1 == s3:
+elif s1 == s2 == s3:
     print('The segments above can form an Equilateral Triangle!')
 elif s1 == s2 or s1 == s3 or s2 == s3:
     print('The segments above can form an Isosceles Triangle! ')
-elif s1 != s2 and s1 != s3 and s2 != s3:
+elif s1 != s2 != s3 != s1:
     print('The segments above can form a Scalene Triangle!')
