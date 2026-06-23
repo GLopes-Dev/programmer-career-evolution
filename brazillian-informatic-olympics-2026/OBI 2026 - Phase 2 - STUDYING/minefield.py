d42d715ca2f19e75c578bd68d439debab220ec43
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-23 13:15:30 -0300

    Solved: matrix manipulation

--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/minefield.py
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/minefield.py
@@ -1,12 +1,29 @@
-N = int(input())
+import sys
+
+M, N = map(int, input().split())
 matriz = []
-matrizR = []
-for i in range(N):
-    A = list(map(int, input().split()))
-    matriz.append(A)
+for _ in range(M):
+    matriz.append(list(input().split()))
 
-print(matriz)
+direcoes = [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1),           (0, 1),
+    (1, -1),  (1, 0),  (1, 1)
+]
 
-for l in range(N):
+for l in range(M):
     for c in range(N):
-        
+        if matriz[l][c] == '*':
+            continue
+        bombas = 0
+        for d_linha, d_coluna in direcoes:
+            vizinho_l = l + d_linha
+            vizinho_c = c + d_coluna
+            if 0 <= vizinho_l < M and 0 <= vizinho_c < N:
+                if matriz[vizinho_l][vizinho_c] == '*':
+                    bombas += 1
+
+        matriz[l][c] = str(bombas)
+
+for linha in matriz:
+    print(" ".join(linha))
