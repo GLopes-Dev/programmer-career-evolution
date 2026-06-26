2d18397a84f427fbc2f791958f373f2312b42736
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-26 14:33:09 -0300

    Life code

---
+++ mext/mext.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+year = datetime.now().year
+if year == 2028:
+    mext_docs1_approved = True
+    mext_exam_approved = True
+    mext_interview_approved = True
+    mext_docs2_approved = True
+    mext_scholar = True
+
+
+if year == 2029 and month == 'April':
+    bags_done = True  
+    passport_docs_visa_done = True
+    airport = True
+    in_plane = True
+    in_Japan = True
+    current_country = 'Japan' 
+
+    japan_mext_scholar_arc()
