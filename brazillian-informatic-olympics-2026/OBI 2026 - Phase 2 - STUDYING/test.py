757c32df21b279127bfc744b5cdda22e6a7acf40
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-27 14:59:44 -0300

    test

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/test.py
@@ -0,0 +1,15 @@
+# pos = [
+#     (1, -1), (-1, 1)
+# ]
+# pos1 = 3
+# pos2 = 3
+# for d_direcao, l_direcao in pos:
+#     vizinho_d = pos1 + d_direcao
+#     vizinho_l = pos2 - l_direcao
+#     print(vizinho_d, vizinho_l)
+
+template = "ABCDDEDEDC"
+print(template[0])
+print(len(template))
+for op in range(len(template)):
+    print(template[op])
