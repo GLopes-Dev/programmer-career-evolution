9689852499b50dbea96a692ca50fabfc443352ae
GLopes-Dev <gsflopes2017@gmail.com>
2026-06-30 11:44:45 -0300

    modified

--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/DFS.py
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/DFS.py
@@ -17,11 +17,40 @@
 
 # dfs('Pedro')
 
-cities = {
-    'Saquarema': ['Rio de Janeiro', 'Niteroi'],
-    'Rio de Janeiro': ['Saquarema', 'Cabo Frio'],
-    'Niteroi': ['Saquarema'],
-    'Cabo Frio': ['Rio de Janeiro']
+# cities = {
+#     'Saquarema': ['Rio de Janeiro', 'Niteroi'],
+#     'Rio de Janeiro': ['Saquarema', 'Cabo Frio'],
+#     'Niteroi': ['Saquarema'],
+#     'Cabo Frio': ['Rio de Janeiro']
+# }
+
+# print(f'Saquarema neighbours: {', '.join(cities["Saquarema"])}')
+
+
+# connections = {
+#     'Biel': ['MEXT_Monbukagakusho', 'Cla_Roblox'],
+#     'Cla_Roblox': ['Biel'],
+#     'MEXT_Monbukagakusho': []
+# }
+
+connections = {
+    'Biel': ['MEXT_Monbukagakusho', 'Cla_Roblox'],
+    'Cla_Roblox': ['Biel'],
+    'MEXT_Monbukagakusho': [],
+    'Guanabara': ['Biel']
 }
 
-print(f'Saquarema neighbours: {', '.join(cities["Saquarema"])}')
+visitados = set()
+
+def dfs(no_atual, grafo):
+    if no_atual in visitados:
+        return
+    
+    print(f"Visitando: {no_atual}")
+    visitados.add(no_atual)
+
+    for vizinhos in grafo[no_atual]:
+        dfs(vizinhos, grafo)
+
+print("--- Starting DFS ---")
+dfs('Guanabara', connections)
