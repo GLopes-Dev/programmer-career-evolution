e055c7df970f0543bddf6e95aca528f219faa46b
GLopes-Dev <gsflopes2017@gmail.com>
2026-07-02 10:40:24 -0300

    Solving

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/dfs-exercise.py
@@ -0,0 +1,17 @@
+import sys
+
+def tem_ciclo(atual, conexoes, visitados):
+    if atual in visitados:
+        return True
+    visitados.add(atual)
+    for vizinhos in conexoes[atual]:
+        if
+    
+conexoes = {}
+visitados = set()
+
+N, M = map(int, sys.stdin.readline().split())
+for l in range(M):
+    X, Y = sys.stdin.readline().split()
+    conexoes[X] = Y
+servidor_inicial = sys.stdin.readline()
