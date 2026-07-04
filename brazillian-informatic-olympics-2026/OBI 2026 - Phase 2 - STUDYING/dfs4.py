e6ec450a7d4f908bb02ec2455389c9885bce9c34
GLopes-Dev <gsflopes2017@gmail.com>
2026-07-04 18:13:19 -0300

    Solved: dfs

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/dfs4.py
@@ -0,0 +1,30 @@
+P, T = map(int, input().split())
+altitude = [0] + list(map(int, input().split()))
+caminhos = {}
+for p in range(1, P+1):
+    caminhos[p] = []
+for p in range(P):
+    I, J = map(int, input().split())
+    caminhos[I].append(J)
+    caminhos[J].append(I)
+start = int(input())
+visitados = set()
+
+def rotas(atual, caminhos, altura, visitados):
+    if atual in visitados:
+        return 0
+    maximo_daqui = 0
+    visitados.add(atual)
+
+    for rota in caminhos[atual]:
+        if altura[rota] < altura[atual]:
+            maximo_da_proxima = 1 + rotas(rota, caminhos, altura, visitados)
+            if maximo_da_proxima > maximo_daqui:
+                maximo_daqui = maximo_da_proxima
+
+    visitados.remove(atual)
+    return maximo_daqui
+
+    
+
+print(rotas(start, caminhos, altitude, visitados))
