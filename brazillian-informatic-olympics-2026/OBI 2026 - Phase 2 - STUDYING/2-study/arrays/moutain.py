6aec9e20254fefd6cae538356006fe2c4b043f4f
GLopes-Dev <gsflopes2017@gmail.com>
2026-07-18 13:17:23 -0300

    arrays manipulation

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/2-study/arrays/moutain.py
@@ -0,0 +1,18 @@
+# N = int(input())
+# alturas = list(map(int, input().split()))
+# picos = 0
+# for a in range(N):
+#     if 0 < a < N:
+#         if alturas[a - 1] < alturas[a] > alturas[a + 1]:
+#             picos += 1
+# print(picos)
+
+#Alterando indice do loop
+
+N = int(input())
+alturas = list(map(int, input().split()))
+picos = 0
+for a in range(1, N-1):
+    if alturas[a-1] < alturas[a] > alturas[a + 1]:
+        picos += 1
+print(picos)
