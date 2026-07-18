8a87f61296f52da0f9efaec5f4a4e122590f37ec
GLopes-Dev <gsflopes2017@gmail.com>
2026-07-18 13:20:37 -0300

    arrays manipulation

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/2-study/2d-array/minefield-again.py
@@ -0,0 +1,6 @@
+N, M = map(int, input().split())
+matriz = [list(map(int, input().split())) for _ in range(N)]
+nova_matriz = [[0] * M for _ in range(N)]
+print(matriz)
+print("--------")
+print(nova_matriz)
--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/2-study/arrays/moutain.py
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/2-study/arrays/moutain.py
@@ -1,11 +1,11 @@
-# N = int(input())
-# alturas = list(map(int, input().split()))
-# picos = 0
-# for a in range(N):
-#     if 0 < a < N:
-#         if alturas[a - 1] < alturas[a] > alturas[a + 1]:
-#             picos += 1
-# print(picos)
+N = int(input())
+alturas = list(map(int, input().split()))
+picos = 0
+for a in range(N):
+    if 0 < a < N:
+        if alturas[a - 1] < alturas[a] > alturas[a + 1]:
+            picos += 1
+print(picos)
 
 #Alterando indice do loop
 
