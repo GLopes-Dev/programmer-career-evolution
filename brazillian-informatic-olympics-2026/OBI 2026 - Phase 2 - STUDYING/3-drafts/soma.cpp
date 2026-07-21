de9c05dbeb00f30a67857e3fbae4e6c33085c86f
GLopes-Dev <gsflopes2017@gmail.com>
2026-07-21 09:18:34 -0300

    C++ Training

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/3-drafts/soma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int resultado = a + b;
+    
+    cout << "The sum is: " << resultado << endl;
+
+    return 0;
+}
---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/3-drafts/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1 = 10;
+    int n2 = 2;
+    if (n1 + n2 == 14) {
+        cout << "Hello, World!" << endl;
+    }
+    else if (n1 + n2 == 10) {
+        cout << "Ola, Mundo!" << endl;
+    }
+    else {
+        cout << "seiko, konnichiwa" << endl;
+    }
+    return 0;
+}
