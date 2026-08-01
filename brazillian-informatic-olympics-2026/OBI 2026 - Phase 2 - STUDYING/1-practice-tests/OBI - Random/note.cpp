57e88d4c8e65591e329c99dccedd827e4f84be47
GLopes-Dev <gsflopes2017@gmail.com>
2026-08-01 10:39:27 -0300

    Solved: math

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/1-practice-tests/OBI - Random/note.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, M;
+    cin >> A >> M;
+    int B = 2 * M - A;
+    cout << B;
+
+    return 0;
+}
--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 2 - STUDYING/1-practice-tests/OBI - Random/note.py
+++
@@ -1,4 +0,0 @@
-A = int(input())
-M = int(input())
-B = 
-(100 + x) / 2 = 70
