957ff69e3fb502a3a2bf654e0caf1413f080b35e
GLopes-Dev <gsflopes2017@gmail.com>
2026-09-08 12:10:36 -0300

    Solved: Index Map / Reverse / Math

--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 3 - Studying/practice-exercises/olympic-party.py
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 3 - Studying/practice-exercises/olympic-party.py
@@ -1,23 +1,32 @@
 N = int(input())
-suditos = [n for n in range(1, N+1)]
 M = int(input())
-for turnos in range(M):
+turnos = []
+suditos_final = []
+
+for _ in range(M):
     t_atual = int(input())
-    quantidade = N - (N // t_atual)
-    suditos = [s for idx, s in enumerate(suditos) if (idx + 1) % t_atual != 0]
+    turnos.append(t_atual)
+    removidos = N // t_atual
+    N = N - removidos
 
-if len(suditos) > 10000:
-    for sudito in range(10000):
-        print(suditos[sudito])
-else:
-    for sudito in suditos:
-        print(sudito)
+for p in range(1, min(N, 10000) + 1):
+    for turno in reversed(turnos):
+        p = p + (p - 1) // (turno - 1)
+    suditos_final.append(p)
+
+for sudito in suditos_final:
+    print(sudito)
 
 
 
-# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#  1  2  3  4  5  6  7  8  9  10
 
-# Depois:
-# [1, 2, 4, 5, 7, 8, 10]
-#  1  2  3  4  5  6  7
+
+
+
+
+
+
+
+
+
+    ## Pq total dividido por turno da exatamente a qtd de removidos?
