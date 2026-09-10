e4d614ab5a59fd04f00504c53a39a8565bcc6a96
GLopes-Dev <gsflopes2017@gmail.com>
2026-09-10 12:09:18 -0300

    Solved: with helps

--- brazillian-informatic-olympics-2026/OBI 2026 - Phase 3 - Studying/practice-exercises/olympic-party copy.py
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 3 - Studying/practice-exercises/olympic-party copy.py
@@ -1,27 +1,32 @@
 import sys
 from bisect import bisect_left
-N = int(sys.stdin.buffer.readline())
-M = int(sys.stdin.buffer.readline())
-turnos = []
 
-for _ in range(M):
-    t_atual = int(sys.stdin.buffer.readline())
-    turnos.append(t_atual)
-    removidos = N // t_atual
-    N -= removidos
+def main():
+    N = int(sys.stdin.buffer.readline())
+    M = int(sys.stdin.buffer.readline())
+    turnos = []
 
-posicoes = [n for n in range(1, min(N, 10000) + 1)]
-total_posicoes = len(posicoes)
-for turno in reversed(turnos):
-        inicio = bisect_left(posicoes, turno)
-        if inicio == len(posicoes):
-             continue
-        d = turno - 1
-        for i in range(inicio, total_posicoes):
-            p = posicoes[i]
-            posicoes[i] = p + (p - 1) // d
+    for _ in range(M):
+        t_atual = int(sys.stdin.buffer.readline())
+        turnos.append(t_atual)
+        removidos = N // t_atual
+        N -= removidos
 
-sys.stdout.write("\n".join(map(str, posicoes)))
+    posicoes = [n for n in range(1, min(N, 10000) + 1)]
+    total_posicoes = len(posicoes)
+    for turno in reversed(turnos):
+            inicio = bisect_left(posicoes, turno)
+            if inicio == total_posicoes:
+                continue
+            d = turno - 1
+            for i in range(inicio, total_posicoes):
+                p = posicoes[i]
+                posicoes[i] = p + (p - 1) // d
+
+    sys.stdout.write("\n".join(map(str, posicoes)))
+
+if __name__ == "__main__":
+     main()    
         
 
 
