5ec6d9a52743ca936c6ae429411f41b8ca295dc9
GLopes-Dev <gsflopes2017@gmail.com>
2026-09-26 09:46:36 -0300

    Solved: dijkstra

---
+++ brazillian-informatic-olympics-2026/OBI 2026 - Phase 3 - Studying/dijkstra/delivery-route.py
@@ -0,0 +1,32 @@
+import heapq as hp
+
+N, M = map(int, input().split())
+grafo = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    A, B, T = map(int, input().split())
+    grafo[A].append((B, T))
+    grafo[B].append((A, T))
+
+temp = [float('inf')] * (N+1)
+temp[1] = 0
+
+fila = [(0, 1)]
+
+while fila:
+    tempo, cidade = hp.heappop(fila)
+
+    if tempo > temp[cidade]:
+        continue
+
+    for vizinho, t in grafo[cidade]:
+        nova_distancia = t + tempo
+
+        if nova_distancia < temp[vizinho]:
+            temp[vizinho] = nova_distancia
+            hp.heappush(fila, (nova_distancia, vizinho))
+
+if temp[N] == float('inf'):
+    print(-1)
+else:
+    print(temp[N])
